8250e11cba0b3cb5ae6450419b87daa7bf970832
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-08 16:37:21 -0300

    feat: adds UserController

---
+++ src/main/java/com/joao/ecommerce/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.joao.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String getMessage() {
+        return "Spring Boot is working";
+    }
+
+}
