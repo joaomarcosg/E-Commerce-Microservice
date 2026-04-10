3eaa012cf3e31d9271ad6ebd7a5103ca606a856c
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-04-10 15:36:16 -0300

    feat: adds validation annotations

--- src/main/java/com/joao/ecommerce/dto/UserDTO.java
+++ src/main/java/com/joao/ecommerce/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.joao.ecommerce.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "CPF is required")
     private String cpf;
     private String address;
+    @NotBlank(message = "Email is required")
     private String email;
     private String phone;
     private LocalDateTime registrationDate;
